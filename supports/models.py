961a7e70fc6f220ff9052db0844095a58b5f75ce
SP <coloviev2@gmail.com>
2025-11-24 12:52:32 +0300

    1
    1

---
+++ supports/models.py
@@ -0,0 +1,27 @@
+import uuid
+
+from django.db import models
+from django.db.models import ForeignKey
+
+from users.models import Contracts, User
+
+
+class SupporTicket(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    contract = models.ForeignKey(Contracts, related_name='contract', blank=True,on_delete=models.CASCADE)
+    responsible_id = ForeignKey(User, related_name='supporter', blank=True,on_delete=models.CASCADE)
+    asker_id = models.ForeignKey(User, related_name='asker', blank=True,on_delete=models.CASCADE)
+    description = models.TextField()
+    status = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description
+
+class Support_message(models.Model):
+    ticket_id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    message = models.CharField(max_length=255)
+    date_message = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
---
+++ users/models.py
@@ -0,0 +1,97 @@
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group
+from django.db import models
+from django.db.models import ForeignKey, OneToOneField
+
+
+# Создание менеджера пользователей
+class MyUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("User must have a login")
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Суперпользователь должен иметь is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Суперпользователь должен иметь is_superuser=True.')
+
+        return self.create_user(username, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    login = models.CharField(max_length=255, unique=True, verbose_name='Логин')
+    password = models.CharField(max_length=128) # паоль хешиоруется на стороке бекенда
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    email = models.CharField(max_length=255)
+    message = models.TextField()
+    group = models.TextField()
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'login'
+
+    def __str__(self):
+        return self.login
+
+class Company(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    INN = models.BigIntegerField()
+    OGRN = models.BigIntegerField()
+    name = models.CharField(max_length=255)
+    founder = models.ManyToManyField(User, related_name='company')
+
+    def __str__(self):
+        return self.name
+
+class Contracts(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    company = models.UUIDField(ForeignKey(Company, on_delete=models.CASCADE))
+    name = models.CharField(max_length=255)
+    documment = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Transactions(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    contract = models.ForeignKey(Contracts, on_delete=models.CASCADE)
+    ammount = models.DecimalField(max_digits=10, decimal_places=2)
+    type_transaction = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    sender_agreement = models.UUIDField(null=True)
+    recipient_agreement = models.UUIDField(null=True)
+    data_trasaction = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.type_transaction
+
+class Application(models.Model):
+    owner = models.IntegerField(ForeignKey(User, on_delete=models.CASCADE, related_name='application_owner'))
+    type_request = models.CharField(max_length=255)
+    message = models.TextField()
+    for_company = ForeignKey(Company, on_delete=models.CASCADE, related_name='application_for_company')
+    for_contract = ForeignKey(Contracts, on_delete=models.CASCADE, related_name='application_for_contract')
+    application_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.type_request
+
+class ApplicationCheck(models.Model):
+    application_id = models.IntegerField(OneToOneField(Application, on_delete=models.CASCADE))
+    approved = models.IntegerField()
+    action = models.CharField(max_length=255)
+    application_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.application_id
+
